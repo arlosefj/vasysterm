3fe031b360cd5a49cede56a4a7da391cf28e9c19
arlosefj <arlose@zstu.edu.cn>
2018-05-23 11:58:23 +0800

    add file

---
+++ analyseServer/ASregister.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+# 使用配置文件存储管理服务器信息，通过握手机制将服务器添加到管理服务器数据库中
+if __name__ == "__main__":
+    print "register analyse server"
---
+++ analyseServer/ASstatus.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+# 定时上报服务器状态，服务器GPU使用情况，显存剩余情况
+if __name__ == "__main__":
+    print "send analyse server status to manageserver"
